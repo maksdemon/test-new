b87a3a68ecce7b8e11c35ed4b20ad10d478ae298
mw <example@worl.com>
2020-11-05 22:59:56 +0300

    home4

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,17 @@
-let answer = confirm("ВАм есть 18");
-if (answer == true) {
-console.log("Проходите")
-} else {
-console.log("Уходи")
-}
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/symbol/1.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/symbol/2.png"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+              dots: true,
+              arrows: false
+            }
+        }         
+        ]
+    });
+  });
+          
